e600074829d91a1aaf2518ee121abf5f36fef57a
Loc Cao <loccb90@gmail.com>
2018-10-12 11:57:44 +0700

    initial commit

---
+++ src/routes/Investors/index.js
@@ -0,0 +1,65 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'dva';
+import { Table, Badge } from 'antd';
+import MainLayout from '../../components/MainLayout/MainLayout';
+import { numberWithCommas } from '../../utils/numbers';
+import BuyTokenDrawer from './BuyTokenDrawer';
+
+class Investors extends PureComponent {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({ type: 'investors/fetch', payload: {} });
+  }
+
+  render() {
+    const { transactions, loading, location } = this.props;
+    const columns = [
+      {
+        title: 'description',
+        dataIndex: 'description',
+        key: 'description',
+      },
+      {
+        title: 'time',
+        dataIndex: 'time',
+        key: 'time',
+      },
+      {
+        title: 'VND',
+        dataIndex: 'VND',
+        key: 'vnd',
+        render: (t, r) => <span>{`${numberWithCommas(r.VND)} VND`}</span>,
+      },
+      {
+        title: 'status',
+        dataIndex: 'status',
+        key: 'status',
+        render: (t, r) => <span><Badge status={r.status === 'Confirmed' ? 'success' : 'default'} />{t}</span>,
+      },
+    ];
+
+    return (
+      <MainLayout location={location}>
+        <div style={{ padding: 24 }}>
+          <h1>Transactions</h1>
+          <BuyTokenDrawer />
+          <Table
+            loading={loading}
+            rowKey={record => record.description}
+            columns={columns}
+            dataSource={transactions}
+          />
+        </div>
+      </MainLayout>
+    );
+  }
+}
+
+const mapStateToProps = ({ loading, investors }) => {
+  return {
+    loading: loading.models.investors,
+    ...investors,
+  };
+};
+
+export default connect(mapStateToProps)(Investors);
